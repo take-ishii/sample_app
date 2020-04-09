072bd030584aa15bc6a81486aa18274442f4a69c
take-ishii <t_ishii@timedia.co.jp>
2020-04-09 06:00:18 +0000

    ログイン後にユーザー情報のページにリダイレクト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) # ユーザーがデータベースにあり、かつ、認証に成功した場合にのみ
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user # redirect_to user_url(user)と等価
     else
       flash.now[:danger] = 'Invalid email/password combination' # flash.nowはレンダリングが終わっているページでフラッシュメッセージを表示、その後リクエストが発生したときに消滅
       render 'new'
