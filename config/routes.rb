a470c00b7e4ced7f69d27ceeca7e574e11e429be
take-ishii <t_ishii@timedia.co.jp>
2020-04-09 02:03:38 +0000

    ログインフォーム追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/login',   to: 'sessions#new'         # 新しいセッションのページ（ログイン） "/login"へアクセスされた際に"session#new"アクションを実行
+  post   '/login',   to: 'sessions#create'      # 新しいセッションの作成（ログイン） "session#create"アクションの情報を"/login"へ送信
+  delete '/logout',  to: 'sessions#destroy'     # セッションの削除（ログアウト）
   resources :users
 end
