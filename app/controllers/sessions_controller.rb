c158588bb494110a21b6754fad83d7b4735d411e
take-ishii <t_ishii@timedia.co.jp>
2020-04-09 08:56:44 +0000

    ユーザーの記憶

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password]) # ユーザーがデータベースにあり、かつ、認証に成功した場合にのみ
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
+      remember user # ユーザーを保持
       redirect_to user # redirect_to user_url(user)と等価
     else
       flash.now[:danger] = 'Invalid email/password combination' # flash.nowはレンダリングが終わっているページでフラッシュメッセージを表示、その後リクエストが発生したときに消滅
