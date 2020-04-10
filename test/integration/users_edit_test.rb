37fa9f6de2e6e6c48a1189c6f25ead2344e40568
take-ishii <t_ishii@timedia.co.jp>
2020-04-10 08:45:15 +0000

    編集の成功に対するテスト

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit' # editビューが表示されているか確認
+    patch user_path(@user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+
+    assert_template 'users/edit'
+  end
+  
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload # データベースから最新のユーザー情報を読み込み直す
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+end
