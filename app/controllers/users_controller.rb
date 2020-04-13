39e1865d5f8e1d0b0060300f5a96d3da0e5857c2
take-ishii <t_ishii@timedia.co.jp>
2020-04-13 05:09:38 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update] # なんらかの処理が実行される直前に特定のメソッドを実行.onlyオプションをかけることでeditとupdateのみに制限
+
   def show
     @user = User.find(params[:id])
   end
@@ -37,5 +39,15 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
