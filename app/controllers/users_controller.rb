0ca50ba73cb3a734cc9704ad975ee788b189639f
take-ishii <t_ishii@timedia.co.jp>
2020-04-13 06:42:44 +0000

    beforeバグとビューファイル名修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index] # なんらかの処理が実行される直前に特定のメソッドを実行.onlyオプションをかけることでeditとupdateのみに制限
-  before_action :correct_user,   only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:index, :edit, :update] # なんらかの処理が実行される直前に特定のメソッドを実行.onlyオプションをかけることでeditとupdateのみに制限
+  before_action :correct_user,   only: [:edit, :update]
 
   def index
     @users = User.all
@@ -39,25 +39,25 @@ class UsersController < ApplicationController
   
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-    
-    # beforeアクション
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  
+  # beforeアクション
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
-    
-    # 正しいユーザーかどうか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+  end
+  
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
