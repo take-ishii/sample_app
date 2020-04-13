9d8da60b2494fcb2cc58b8766522b2516a40c1bc
take-ishii <t_ishii@timedia.co.jp>
2020-04-13 05:23:58 +0000

    current_user?メソッド追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,9 +49,10 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+    
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
