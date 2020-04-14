888da8992aacf9dfd2a51b2fecbae6d6eb747abc
take-ishii <t_ishii@timedia.co.jp>
2020-04-14 02:20:34 +0000

    destroyテスト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy] # なんらかの処理が実行される直前に特定のメソッドを実行.onlyオプションをかけることでeditとupdateのみに制限
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
 #    @users = User.all # こちらはユーザー全員の一括表示
@@ -69,5 +70,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
