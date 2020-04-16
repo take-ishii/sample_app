5030a0f1ed698e04b66d4000af9cb02d27051510
take-ishii <t_ishii@timedia.co.jp>
2020-04-16 09:50:45 +0000

    following関連のメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
   
   attr_accessor :remember_token, :activation_token, :reset_token # 仮想の属性を生成
   before_save   :downcase_email
@@ -80,6 +81,21 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
   
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     # メールアドレスをすべて小文字にする
