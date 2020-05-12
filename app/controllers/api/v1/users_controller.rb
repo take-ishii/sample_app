330667b111701aae9a19e58b5d7009e7ddbc5496
take-ishii <t_ishii@timedia.co.jp>
2020-05-12 06:31:39 +0000

    レスポンスをユーザー名、アイコン画像アップロード先URL、マイクロポスト内容に

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,10 @@ module Api
     class UsersController < ApplicationController
       def get_microposts
         @user = User.find params[:id]
-        microposts = @user.microposts
-        render json: { data: microposts.paginate(page: params[:page]) }
+        @microposts = @user.microposts
+        gravatar_id = Digest::MD5::hexdigest(@user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
+        render json: [user_name: @user.name, icon_url: gravatar_url, microposts: @microposts].to_json
       end
     end
   end
