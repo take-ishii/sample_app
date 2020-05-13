20d367a7febe2f39fb1d173fd3a6e09cdc249bf1
take-ishii <t_ishii@timedia.co.jp>
2020-05-13 06:43:36 +0000

    インスタンス変数をローカル変数に

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,11 @@ module Api
   module V1
     class UsersController < ApplicationController
       def get_microposts
-        @user = User.find params[:id]
-        @microposts = @user.microposts
-        gravatar_id = Digest::MD5::hexdigest(@user.email.downcase)
+        user = User.find params[:id]
+        microposts = user.microposts
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
-        render json: {user_name: @user.name, icon_url: gravatar_url, microposts: @microposts}.to_json
+        render json: {user_name: @user.name, icon_url: gravatar_url, microposts: microposts}.to_json
       end
     end
   end
