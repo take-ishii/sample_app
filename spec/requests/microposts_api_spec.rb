d5651163806e56324f30daa392cb9ffd27076e84
take-ishii <t_ishii@timedia.co.jp>
2020-05-14 06:42:04 +0000

    存在しないIDに対してのテスト追加 RED

--- spec/requests/microposts_api_spec.rb
+++ spec/requests/microposts_api_spec.rb
@@ -8,22 +8,36 @@ RSpec.describe "microposts api", type: :request do
     let!(:micropost) { create_list(:user_post,200, user: user) }
     subject(:json) { JSON.parse(response.body) }  
     
-    before { get "/api/v1/users/#{user.id}/microposts" }
-    
-    it "リクエストが成功していること" do
-      expect(response.status).to eq(200)
+    context "ユーザーIDが存在している場合" do
+      before { get "/api/v1/users/#{user.id}/microposts" }
+
+      it "リクエストが成功していること" do
+        expect(response.status).to eq(200)
+      end
+      
+      it "ユーザー名を取得していること" do
+        expect(json["user_name"]).to eq(user.name)
+      end    
+      
+      it "アイコンURLを取得していること" do
+        expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
+      end    
+      
+      it "生成した全てのマイクロポストを取得していること" do
+        expect(json["microposts"].length).to eq(200)
+      end
     end
     
-    it "ユーザー名を取得していること" do
-      expect(json["user_name"]).to eq(user.name)
-    end    
-    
-    it "アイコンURLを取得していること" do
-      expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
-    end    
-    
-    it "生成した全てのマイクロポストを取得していること" do
-      expect(json["microposts"].length).to eq(200)
+    context "ユーザーIDが存在していない場合" do
+      before { get "/api/v1/users/500/microposts" }
+      
+      it "リクエストが成功していること" do
+        expect(response.status).to eq(200)
+      end
+      
+      it "エラーメッセージが返ってきていること" do
+        expect(response.body).to eq('{"message":"Validation Failed"}')   
+      end
     end
   end
 end
