602fec97b50d5702b006aafaa04567c239df75a3
take-ishii <t_ishii@timedia.co.jp>
2020-05-14 07:11:12 +0000

    膨大な投稿数があるユーザーに対してのテスト

--- spec/requests/microposts_api_spec.rb
+++ spec/requests/microposts_api_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe "microposts api", type: :request do
   describe "特定ユーザーのマイクロポストを取得する" do
     let!(:user) { create(:user) }
     let!(:zeropost_user) { create(:user) }
+    let!(:manypost_user) { create(:user) }
     let!(:micropost) { create_list(:user_post,200, user: user) }
+    let!(:many_micropost) { create_list(:user_post,10000, user: manypost_user) }
     subject(:json) { JSON.parse(response.body) }  
     
     context "投稿があるユーザーの場合" do
@@ -49,6 +51,25 @@ RSpec.describe "microposts api", type: :request do
       end
     end
     
+    context "膨大な投稿があるユーザーの場合" do
+      before { get "/api/v1/users/#{manypost_user.id}/microposts" }
+
+      it "リクエストが成功していること" do
+        expect(response.status).to eq(200)
+      end
+      
+      it "ユーザー名を取得していること" do
+        expect(json["user_name"]).to eq(user.name)
+      end    
+      
+      it "アイコンURLを取得していること" do
+        expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(manypost_user.email.downcase)}?s=80")
+      end    
+      
+      it "生成した全てのマイクロポストを取得していること" do
+        expect(json["microposts"].length).to eq(10000)
+      end
+    end
     
     context "ユーザーIDが存在していない場合" do
       before { get "/api/v1/users/500/microposts" }
