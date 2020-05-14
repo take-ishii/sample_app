7a865c4f326ce3dcdb49e66b340791af43d369a9
take-ishii <t_ishii@timedia.co.jp>
2020-05-14 06:44:52 +0000

    userが存在していなければエラーメッセージを返す GREEN

--- app/controllers/api/v1/users/microposts_controller.rb
+++ app/controllers/api/v1/users/microposts_controller.rb
@@ -3,10 +3,15 @@ module Api
     module Users
       class MicropostsController < ApplicationController
         def index
-          user = User.find params[:user_id]
-          gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-          gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
-          render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts}
+          user = User.find_by(id: params[:user_id])
+          
+          if user
+            gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+            gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
+            render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts}
+          else
+            render json: {message: "Validation Failed"}            
+          end
         end
       end
     end
