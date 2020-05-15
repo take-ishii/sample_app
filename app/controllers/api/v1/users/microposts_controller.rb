a79816ba67838536e9a5a56326da354e1fa6264c
take-ishii <t_ishii@timedia.co.jp>
2020-05-15 05:23:36 +0000

    不要になった条件分岐を削除

--- app/controllers/api/v1/users/microposts_controller.rb
+++ app/controllers/api/v1/users/microposts_controller.rb
@@ -6,13 +6,9 @@ module Api
         def index
           user = User.find(params[:user_id])  
 
-          if user
-            gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-            gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
-            render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts}
-          else
-            render json: {message: "Validation Failed"}            
-          end
+          gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+          gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
+          render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts}
           
           rescue ActiveRecord::RecordNotFound
             response.status = 404
