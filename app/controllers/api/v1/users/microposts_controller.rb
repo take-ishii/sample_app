879f092707eab9bb6e5f31d6b90b6696895ad107
take-ishii <t_ishii@timedia.co.jp>
2020-05-18 05:41:31 +0000

    ページネーション実装

--- app/controllers/api/v1/users/microposts_controller.rb
+++ app/controllers/api/v1/users/microposts_controller.rb
@@ -8,7 +8,7 @@ module Api
 
           gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
           gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
-          render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts}
+          render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts.paginate(page: params[:page])}
           
         rescue ActiveRecord::RecordNotFound
           response.status = 404
