54cbed73845282904740d2c226b34ea5b41b8670
take-ishii <t_ishii@timedia.co.jp>
2020-05-18 07:14:29 +0000

    perを指定することで表示件数を切り替えられるように　クエリが空だと30件

--- app/controllers/api/v1/users/microposts_controller.rb
+++ app/controllers/api/v1/users/microposts_controller.rb
@@ -5,10 +5,12 @@ module Api
 
         def index
           user = User.find(params[:user_id])  
+          per_page = params[:per].nil? ? 30 : [params[:per].to_i, 100].min
+          microposts = user.microposts.paginate(page: params[:page], per_page: per_page)
 
           gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
           gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
-          render json: {user_name: user.name, icon_url: gravatar_url, microposts: user.microposts.paginate(page: params[:page])}
+          render json: {user_name: user.name, icon_url: gravatar_url, microposts: microposts}
           
         rescue ActiveRecord::RecordNotFound
           response.status = 404
