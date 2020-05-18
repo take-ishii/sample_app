6178c143b7b6ec268915c664ed1a217c0867b039
take-ishii <t_ishii@timedia.co.jp>
2020-05-18 06:20:48 +0000

    テスト実装、１ページ目と最終ページのmicropost数を確認　GREEN

--- spec/requests/microposts_api_spec.rb
+++ spec/requests/microposts_api_spec.rb
@@ -10,28 +10,50 @@ RSpec.describe "microposts api", type: :request do
       let!(:user) { create(:user) }
       
       context "投稿があるユーザーの場合" do
-        let!(:microposts) { create_list(:user_post,2, user: user) }
-        before { get "/api/v1/users/#{user.id}/microposts" }
-  
-        it "リクエストが成功していること" do
-          expect(response.status).to eq(200)
+        let!(:microposts) { create_list(:user_post,32, user: user) }
+        
+        context "1ページ目のmicropostsを取得しようとした場合" do
+          before { get "/api/v1/users/#{user.id}/microposts?page=1" }
+    
+          it "リクエストが成功していること" do
+            expect(response.status).to eq(200)
+          end
+          
+          it "ユーザー名を取得していること" do
+            expect(json["user_name"]).to eq(user.name)
+          end    
+          
+          it "アイコンURLを取得していること" do
+            expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
+          end    
+          
+          it "1ページ目のマイクロポストを取得していること" do
+            expect(json["microposts"].length).to eq([microposts.length,30].min)
+          end
         end
-        
-        it "ユーザー名を取得していること" do
-          expect(json["user_name"]).to eq(user.name)
-        end    
-        
-        it "アイコンURLを取得していること" do
-          expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
-        end    
-        
-        it "生成した全てのマイクロポストを取得していること" do
-          expect(json["microposts"].length).to eq(microposts.length)
+        context "最終ページのmicropostsを取得しようとした場合" do
+          before { get "/api/v1/users/#{user.id}/microposts?page=#{microposts.length/30+1}" }
+    
+          it "リクエストが成功していること" do
+            expect(response.status).to eq(200)
+          end
+          
+          it "ユーザー名を取得していること" do
+            expect(json["user_name"]).to eq(user.name)
+          end    
+          
+          it "アイコンURLを取得していること" do
+            expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
+          end    
+          
+          it "最終ページのマイクロポストを取得していること" do
+            expect(json["microposts"].length).to eq([microposts.length%30,30].min)
+          end
         end
       end
       
       context "投稿がないユーザーの場合" do
-        before { get "/api/v1/users/#{user.id}/microposts" }
+        before { get "/api/v1/users/#{user.id}/microposts?page=1" }
   
         it "リクエストが成功していること" do
           expect(response.status).to eq(200)
@@ -45,35 +67,57 @@ RSpec.describe "microposts api", type: :request do
           expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
         end    
         
-        it "生成した全てのマイクロポストを取得していること" do
+        it "マイクロポストを一つも取得しないこと" do
           expect(json["microposts"].length).to eq(0)
         end
       end
       
       context "膨大な投稿があるユーザーの場合" do
-        let!(:microposts) { create_list(:user_post,10000, user: user) }
-        before { get "/api/v1/users/#{user.id}/microposts" }
+        let!(:microposts) { create_list(:user_post,1000, user: user) }
+
+        context "1ページ目のmicropostsを取得しようとした場合" do
+          before { get "/api/v1/users/#{user.id}/microposts?page=1" }
   
-        it "リクエストが成功していること" do
-          expect(response.status).to eq(200)
+          it "リクエストが成功していること" do
+            expect(response.status).to eq(200)
+          end
+          
+          it "ユーザー名を取得していること" do
+            expect(json["user_name"]).to eq(user.name)
+          end    
+          
+          it "アイコンURLを取得していること" do
+            expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
+          end    
+          
+          it "1ページ目のマイクロポストを取得していること" do
+            expect(json["microposts"].length).to eq([microposts.length,30].min)
+          end
         end
-        
-        it "ユーザー名を取得していること" do
-          expect(json["user_name"]).to eq(user.name)
-        end    
-        
-        it "アイコンURLを取得していること" do
-          expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
-        end    
-        
-        it "生成した全てのマイクロポストを取得していること" do
-          expect(json["microposts"].length).to eq(microposts.length)
+        context "最終ページのmicropostsを取得しようとした場合" do
+          before { get "/api/v1/users/#{user.id}/microposts?page=#{microposts.length/30+1}" }
+    
+          it "リクエストが成功していること" do
+            expect(response.status).to eq(200)
+          end
+          
+          it "ユーザー名を取得していること" do
+            expect(json["user_name"]).to eq(user.name)
+          end    
+          
+          it "アイコンURLを取得していること" do
+            expect(json["icon_url"]).to eq("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=80")
+          end    
+          
+          it "最終ページのマイクロポストを取得していること" do
+            expect(json["microposts"].length).to eq([microposts.length%30,30].min)
+          end
         end
       end
     end
 
     context "ユーザーIDが存在していない場合" do
-      before { get "/api/v1/users/500/microposts" }
+      before { get "/api/v1/users/500/microposts?page=1" }
       
       it "リクエストが失敗していること" do
         expect(response.status).to eq(404)
