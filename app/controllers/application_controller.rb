5f2650dfac78dfe949380370a1a6967af2857e72
take-ishii <t_ishii@timedia.co.jp>
2020-05-20 09:32:54 +0000

    render_404をprivateに

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
 
-  def render_404(exception = nil)
-    if request.format.to_sym == :json
-      render json: { status: 404, error: (exception ? exception.message : 'not_found') }, status: :not_found
-    else
-      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
-    end
-  end
-  
   private
+  
+    def render_404(exception = nil)
+      if request.format.to_sym == :json
+        render json: { status: 404, error: (exception ? exception.message : 'not_found') }, status: :not_found
+      else
+        render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+      end
+    end
   
     # ユーザーのログインを確認する
     def logged_in_user
