ac25153689835426f873cecada59e9a53a1ff908
take-ishii <t_ishii@timedia.co.jp>
2020-05-20 05:32:59 +0000

    APIエラーをapplication_controllerに移動、routing_errorとまとめる。メッセージを変更、ステータスも送るように

--- app/controllers/api/v1/users/microposts_controller.rb
+++ app/controllers/api/v1/users/microposts_controller.rb
@@ -11,10 +11,6 @@ module Api
           gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
           gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=80"
           render json: {user_name: user.name, icon_url: gravatar_url, microposts: microposts}
-          
-        rescue ActiveRecord::RecordNotFound
-          response.status = 404
-          render json: {message: "Validation Failed"}
         end
       end
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
 
+  def render_404(exception = nil)
+    if request.format.to_sym == :json
+      render json: { status: 404, error: (exception ? exception.message : 'not_found') }, status: :not_found
+    else
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    end
+  end
+  
   private
-
+  
     # ユーザーのログインを確認する
     def logged_in_user
       unless logged_in?
