4caf6e769f843de0657196201bdc7ef23d45bdb2
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-27 17:16:32 +0900

    ログインバリデーションAPIのログインしているかを確認する機能を作成

--- app/controllers/api/v1/users/session_validations_controller.rb
+++ app/controllers/api/v1/users/session_validations_controller.rb
@@ -2,7 +2,21 @@ module Api
   module V1
     module Users
 			class SessionValidationsController < ApplicationController
-				
+        def index
+          render_is_logged_in || render_not_logged_in
+        end
+
+        private
+          def render_is_logged_in
+            user = User.find_by(id: params[:user_id])
+            authenticate_with_http_token do |token, options|
+              render json: { is_logged_in: "true" } if user && user.authenticated?(:remember, token)
+						end
+          end
+
+          def render_not_logged_in
+            render json: { is_logged_in: "false" }, status: 401
+          end
 			end
 		end
 	end
