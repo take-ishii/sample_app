3a2fe20fd1991179db643ddfa5018a9f410bf274
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-28 17:48:34 +0900

    利用可能なuser_idとtokenを受け取った時のsession_validationsに対するspec

--- spec/requests/session_validations_spec.rb
+++ spec/requests/session_validations_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "SessionValidations", type: :request do
   describe `index` do
-    context `available account` do
+    let!(:user) { create(:user) }
+    let(:remember_token) { "hogehoge" }
+    context `available user_id and remember_token` do
+      before do
+        user.update_attribute(:remember_digest, User.digest(remember_token))
+      end
+      it `returns 200` do
+        get "/api/v1/users/#{user.id}/session_validations", headers: { Authorization: "Token token=#{remember_token}" }
+        expect(response.status).to eq(200)
+      end
     end
   end
 end
