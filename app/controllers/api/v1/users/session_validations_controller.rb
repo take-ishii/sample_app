d641e2ccdf6c6f58dc7d95f7065f730f4b8c88e7
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-28 12:19:46 +0900

    ログイン判定のコードを修正

--- app/controllers/api/v1/users/session_validations_controller.rb
+++ app/controllers/api/v1/users/session_validations_controller.rb
@@ -3,20 +3,20 @@ module Api
     module Users
       class SessionValidationsController < ApplicationController
         def index
-          render_is_logged_in || render_not_logged_in
+          if logged_in?(params[:user_id])
+            render json: { is_logged_in: 'true' }
+          else
+            render json: { is_logged_in: 'false' }, status: 401
+          end
         end
 
         private
 
-          def render_is_logged_in
-            user = User.find_by(id: params[:user_id])
+          def logged_in?(user_id)
+            user = User.find_by(id: user_id)
             authenticate_with_http_token do |token, _options|
-              render json: { is_logged_in: 'true' } if user&.authenticated?(:remember, token)
+              return user && user.authenticated?(:remember, token)
             end
-          end
-
-          def render_not_logged_in
-            render json: { is_logged_in: 'false' }, status: 401
           end
       end
     end
