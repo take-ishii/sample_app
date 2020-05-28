07b4223c1d28f03f23bed463d766fd66efb434a0
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-05-28 10:49:42 +0900

    session_validations_controllerのフォーマット

--- app/controllers/api/v1/users/session_validations_controller.rb
+++ app/controllers/api/v1/users/session_validations_controller.rb
@@ -1,24 +1,24 @@
 module Api
   module V1
     module Users
-			class SessionValidationsController < ApplicationController
+      class SessionValidationsController < ApplicationController
         def index
           render_is_logged_in || render_not_logged_in
         end
 
         private
-        
+
           def render_is_logged_in
             user = User.find_by(id: params[:user_id])
-            authenticate_with_http_token do |token, options|
-              render json: { is_logged_in: "true" } if user && user.authenticated?(:remember, token)
-						end
+            authenticate_with_http_token do |token, _options|
+              render json: { is_logged_in: 'true' } if user&.authenticated?(:remember, token)
+            end
           end
 
           def render_not_logged_in
-            render json: { is_logged_in: "false" }, status: 401
+            render json: { is_logged_in: 'false' }, status: 401
           end
-			end
-		end
-	end
+      end
+    end
+  end
 end
