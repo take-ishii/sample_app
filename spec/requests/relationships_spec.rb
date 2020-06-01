a580bc6b591197c77c6ed18696ca34fd42aa7d8a
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-06-01 18:35:12 +0900

    rspecのテスト用データとsubjectの定義

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Relationships", type: :request do
-  
+RSpec.describe 'Relationships', type: :request do
+  describe `create` do
+    let!(:user) { create(:user) }
+    let!(:other_user) { create(:other_user) }
+    let(:remember_token) { 'hogehoge' }
+    before do
+      user.update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    subject(:json) { JSON.parse(response.body) }
+  end
 end
