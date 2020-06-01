80c6500110c3a7304a4e6c21739020e7fde2406f
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-06-01 18:36:29 +0900

    フォローに成功した時のspec

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe 'Relationships', type: :request do
       user.update_attribute(:remember_digest, User.digest(remember_token))
     end
     subject(:json) { JSON.parse(response.body) }
+
+    context `available requests and successful to follow` do
+      before do
+        post '/api/v1/relationships', params: { user_id: user.id, followed_id: other_user.id },
+                                      headers: { Authorization: "Token #{remember_token}" }
+      end
+      it `returns 200` do
+        expect(response.status).to eq 200
+      end
+      it `is logged in` do
+        expect(json["is_logged_in"]).to be true
+      end
+      it `is successful to follow` do
+        expect(json["followed"]).to be true
+      end
+    end
   end
 end
