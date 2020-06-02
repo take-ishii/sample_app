6d0d78940534e8390d938136a18489bb3bbd4b4f
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-06-02 12:26:43 +0900

    relationships_controllerにcreateアクションを宣言

--- app/controllers/api/v1/relationships/relationships_controller.rb
+++ app/controllers/api/v1/relationships/relationships_controller.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     module Relationships
       class RelationshipsController < ApplicationController
-        
+        def create
+          
+        end
       end
     end
   end
