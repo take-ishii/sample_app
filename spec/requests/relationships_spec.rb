eddba2c5ca747ca5e6272604d5408f53fa1a652d
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-06-02 10:57:14 +0900

    成功時のログインバリデーションAPIのテストを追加

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -10,36 +10,42 @@ RSpec.describe 'Relationships', type: :request do
     end
     subject(:json) { JSON.parse(response.body) }
 
-    context `available requests and successful to follow` do
-      before do
-        post '/api/v1/relationships', params: { user_id: user.id, followed_id: other_user.id },
-                                      headers: { Authorization: "Token #{remember_token}" }
+    context `available requests` do
+      it `is successful to SessionValidation API` do
+        get "/api/v1/users/#{user.id}/session_validations", headers: { Authorization: "Token token=#{remember_token}" }
+        expect(response.status).to eq(200)
       end
-      it `returns 200` do
-        expect(response.status).to eq 200
+      context 'successful to follow' do
+        before do
+          post '/api/v1/relationships', params: { user_id: user.id, followed_id: other_user.id },
+                                        headers: { Authorization: "Token #{remember_token}" }
+        end
+        it `returns 200` do
+          expect(response.status).to eq 200
+        end
+        it `is logged in` do
+          expect(json['is_logged_in']).to be true
+        end
+        it `is successful to follow` do
+          expect(json['followed']).to be true
+        end
       end
-      it `is logged in` do
-        expect(json["is_logged_in"]).to be true
-      end
-      it `is successful to follow` do
-        expect(json["followed"]).to be true
-      end
-    end
 
-    context `available requests and already followed` do
-      before do
-        Micropost.create(follower_id: user.id, followed_id: other_user.id)
-        post '/api/v1/relationships', params: { user_id: user.id, followed_id: other_user.id },
-                                      headers: { Authorization: "Token #{remember_token}" }
-      end
-      it `returns 200` do
-        expect(response.status).to eq 200
-      end
-      it `is logged in` do
-        expect(json["is_logged_in"]).to be true
-      end
-      it `is already followed` do
-        expect(json["followed"]).to be false
+      context `already followed` do
+        before do
+          Relationship.create(follower_id: user.id, followed_id: other_user.id)
+          post '/api/v1/relationships', params: { user_id: user.id, followed_id: other_user.id },
+                                        headers: { Authorization: "Token #{remember_token}" }
+        end
+        it `returns 200` do
+          expect(response.status).to eq 200
+        end
+        it `is logged in` do
+          expect(json['is_logged_in']).to be true
+        end
+        it `is already followed` do
+          expect(json['followed']).to be false
+        end
       end
     end
 
@@ -52,26 +58,26 @@ RSpec.describe 'Relationships', type: :request do
         expect(response.status).to eq 401
       end
       it `is not logged in` do
-        expect(json["is_logged_in"]).to be false
+        expect(json['is_logged_in']).to be false
       end
       it `is not followed` do
-        expect(json["followed"]).to be false
+        expect(json['followed']).to be false
       end
     end
 
     context `available user_id and available followed_id and unavailable token` do
       before do
         post '/api/v1/relationships', params: { user_id: user.id, followed_id: other_user.id },
-                                      headers: { Authorization: "Token MistakeToken" }
+                                      headers: { Authorization: 'Token MistakeToken' }
       end
       it `returns 401` do
         expect(response.status).to eq 401
       end
       it `is not logged in` do
-        expect(json["is_logged_in"]).to be false
+        expect(json['is_logged_in']).to be false
       end
       it `is not followed` do
-        expect(json["followed"]).to be false
+        expect(json['followed']).to be false
       end
     end
 
@@ -84,10 +90,10 @@ RSpec.describe 'Relationships', type: :request do
         expect(response.status).to eq 404
       end
       it `is logged in` do
-        expect(json["is_logged_in"]).to be true
+        expect(json['is_logged_in']).to be true
       end
       it `is not followed` do
-        expect(json["followed"]).to be false
+        expect(json['followed']).to be false
       end
     end
   end
