c0b6972b42992f2a6e90f7044a8078a7aaaeb394
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-06-02 18:53:21 +0900

    relationshipsのrspecをpending

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Relationships', type: :request do
-  describe `create` do
+  xdescribe `create` do
     let!(:user) { create(:user) }
     let!(:other_user) { create(:other_user) }
     let(:remember_token) { 'hogehoge' }
