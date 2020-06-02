af2dd4304d0430484ffbdf208d96d8752619b99e
Ichikawa Takumi <t_ichikawa@timedia.co.jp>
2020-06-02 12:09:07 +0900

    登録されていないダミーのnot_memberを追加

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Relationships', type: :request do
   describe `create` do
     let!(:user) { create(:user) }
     let!(:other_user) { create(:other_user) }
+    let!(:not_member) { User.new(id: 999, name: 'not_member', email: 'not_member@example.com', remember_digest: User.digest(remember_token)) }
     let(:remember_token) { 'hogehoge' }
     before do
       user.update_attribute(:remember_digest, User.digest(remember_token))
